a071a70e6dec2b034d705da3583b9ef54d7c00cb
adithyaGowda <adithyagowda5696@gmail.com>
2019-08-03 10:39:43 +0530

    array ex

---
+++ src/main/java/com/stackroute/keyEx/ArraysEx.java
@@ -0,0 +1,13 @@
+package com.stackroute.keyEx;
+
+public class ArraysEx {
+
+    public void arrayEx(int[] input){
+
+        int arr[] = new int[input.length];
+        for (int i=0; i<arr.length; i++){
+            arr[i] = input[i]+5;
+            System.out.println("Array element incremented by 5: "+arr[i]);
+        }
+    }
+}
--- src/main/java/com/stackroute/keyEx/MainClass.java
+++ src/main/java/com/stackroute/keyEx/MainClass.java
@@ -4,17 +4,22 @@ public class MainClass {
 
     public static void main(String[] args) {
         //creating object of ConditionalStatementEx class
-        ConditionalStatementEx ex = new ConditionalStatementEx();
-        ex.ifElse(true);//calling ifElse method
-        ex.ifElseIF(2);//calling ifElseIF method
-        ex.nestedIfElse(1);//calling nestedIf method
-        ex.switchEx(3);//calling switchEx method
+//        ConditionalStatementEx ex = new ConditionalStatementEx();
+//        ex.ifElse(true);//calling ifElse method
+//        ex.ifElseIF(2);//calling ifElseIF method
+//        ex.nestedIfElse(1);//calling nestedIf method
+//        ex.switchEx(3);//calling switchEx method
 
-        //creating object of LoopsEx class
-        LoopsEx loopsEx = new LoopsEx();
-        loopsEx.forLoop(5);//calling forLoop method
-        loopsEx.whileLoop(1);//calling whileLoop method
-        loopsEx.doWhileLoop(0);//calling doWhileLoop method
+//        //creating object of LoopsEx class
+//        LoopsEx loopsEx = new LoopsEx();
+//        loopsEx.forLoop(5);//calling forLoop method
+//        loopsEx.whileLoop(1);//calling whileLoop method
+//        loopsEx.doWhileLoop(0);//calling doWhileLoop method
+
+        //creatin object of ArrayEx class
+        ArraysEx arraysEx = new ArraysEx();
+        int[] arr = {1,2,3,4,5};
+        arraysEx.arrayEx(arr);
 
     }
 }
