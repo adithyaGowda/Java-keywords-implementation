1e5c5ddf345b34c3319b4c81828feee17f067ebb
adithyaGowda <adithyagowda5696@gmail.com>
2019-08-03 09:54:04 +0530

    ex for conditional statement

--- src/main/java/com/stackroute/keyEx/ConditionalStatementEx.java
+++ src/main/java/com/stackroute/keyEx/ConditionalStatementEx.java
@@ -3,6 +3,13 @@ package com.stackroute.keyEx;
 public class ConditionalStatementEx {
 
     public static void main(String[] args) {
-        
+        boolean test = true;
+
+        if (test)
+            System.out.println("printing true condition");
+
+        else
+            System.out.println("printing false condition");        
+
     }
 }
